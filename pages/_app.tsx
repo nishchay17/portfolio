af8e8056ca7a05977ca611e36b94cb491856f859
Nishchay <nishchay13971@gmail.com>
2021-10-31 18:57:02 +0530

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,29 @@
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Head from "next/head";
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
 
 import "../styles/globals.css";
 import { theme } from "../lib/theme";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider theme={theme}>
+    <>
       <Head>
         <meta name="description" content="Nishchay17 Portfolio" />
         <meta name="theme-color" content="#469895" />
@@ -31,10 +46,31 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="/favicon-16x16.png"
         />
       </Head>
-      <AnimatePresence>
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </ChakraProvider>
+
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ChakraProvider theme={theme}>
+        <AnimatePresence>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </ChakraProvider>
+    </>
   );
 }
 export default MyApp;
