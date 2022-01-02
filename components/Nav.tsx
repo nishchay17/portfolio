13bc6c833364b66f5f3cd1a9c6b9898427abf60e
Nishchay <nishchay13971@gmail.com>
2022-01-02 22:31:07 +0530

    added animation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,53 +3,60 @@ import Link from "next/link";
 import { Container, Flex, Text } from "@chakra-ui/react";
 
 import ThemeToggleButton from "./ThemeToggleButton";
+import MotionBox from "./MotionBox";
 
 function Nav(): ReactElement {
   return (
     <>
-      <Container maxW="container.xl" py="1rem" px={0}>
-        <nav>
-          <Flex alignItems="center" justifyContent="space-between">
-            <Link href="/">
-              <a>
-                <h1>Nishchay</h1>
-              </a>
-            </Link>
-            <Flex alignItems="center">
-              <a
-                href="https://www.linkedin.com/in/nishchay-trivedi-61219978"
-                target="_blank"
-                rel="noopener noreferrer"
+      <MotionBox
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.7 }}
+        as={Container}
+        maxW="container.xl"
+        py="1rem"
+        px={0}
+      >
+        <Flex as={"nav"} alignItems="center" justifyContent="space-between">
+          <Link href="/">
+            <a>
+              <h1>Nishchay</h1>
+            </a>
+          </Link>
+          <Flex alignItems="center">
+            <a
+              href="https://www.linkedin.com/in/nishchay-trivedi-61219978"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Text
+                sx={{
+                  ":hover": {
+                    color: "teal.800",
+                  },
+                }}
               >
+                Linkedin
+              </Text>
+            </a>
+            <Link href="/#projects">
+              <a>
                 <Text
+                  mx="1rem"
                   sx={{
                     ":hover": {
                       color: "teal.800",
                     },
                   }}
                 >
-                  Linkedin
+                  Projects
                 </Text>
               </a>
-              <Link href="/#projects">
-                <a>
-                  <Text
-                    mx="1rem"
-                    sx={{
-                      ":hover": {
-                        color: "teal.800",
-                      },
-                    }}
-                  >
-                    Projects
-                  </Text>
-                </a>
-              </Link>
-              <ThemeToggleButton />
-            </Flex>
+            </Link>
+            <ThemeToggleButton />
           </Flex>
-        </nav>
-      </Container>
+        </Flex>
+      </MotionBox>
     </>
   );
 }
