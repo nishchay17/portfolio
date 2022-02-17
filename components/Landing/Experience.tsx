f967f57abb906ce533ebc878db17ac96af24896c
nishchay_trivedi <nishchay13971@gmail.com>
2022-02-17 18:49:42 +0530

    feat(experience): added new exp

--- components/Landing/Experience.tsx
+++ components/Landing/Experience.tsx
@@ -2,6 +2,29 @@ import React, { ReactElement } from "react";
 import { Box, Flex, Text, List, ListItem, ListIcon } from "@chakra-ui/react";
 import { FiMinus } from "react-icons/fi";
 
+const exps = [
+  {
+    company: "Homelane",
+    title: "Software Engineer Intern",
+    date: "Jan 2022 - Present",
+    points: [
+      "Worked on developing dashboard using React js and SCSS",
+      "Fixed existing bugs and added functionalities like dashboard popups and API integration",
+    ],
+  },
+  {
+    company: "The Internet Folks",
+    title: "Software Engineer Intern",
+    date: "Dec 2020 - June 2021",
+    points: [
+      "Used Next js (react js) with server-side rendering",
+      "Used Redux (Redux toolkit) for state management",
+      "Converted UI design to reusable, scalable react components",
+      "Implemented Debouncing to reduce API calls",
+    ],
+  },
+];
+
 function Experience(): ReactElement {
   return (
     <Flex flexDirection="column" my="2rem" as="section">
@@ -9,39 +32,41 @@ function Experience(): ReactElement {
         Experience
       </Text>
 
-      <Box width={["100%", "100%", "40rem"]} pl="2rem" position="relative">
-        <Text fontSize="2xl">The Internet Folks</Text>
-        <Text opacity="0.6">
-          Software Engineer Intern <i>Dec 2020 - June 2021</i>
-        </Text>
-        <Box
-          position="absolute"
-          top="9%"
-          left={0}
-          border="6px solid var(--primary-color)"
-          borderRadius="50%"
-          height="1.5rem"
-          width="1.5rem"
-        />
-      </Box>
-      <List spacing={1} ml="3rem" mt="1rem">
-        <ListItem>
-          <ListIcon as={FiMinus} color="green.500" />
-          Used Next js (react js) with server-side rendering
-        </ListItem>
-        <ListItem>
-          <ListIcon as={FiMinus} color="green.500" />
-          Used Redux (Redux toolkit) for state management
-        </ListItem>
-        <ListItem>
-          <ListIcon as={FiMinus} color="green.500" />
-          Converted UI design to reusable, scalable react components
-        </ListItem>
-        <ListItem>
-          <ListIcon as={FiMinus} color="green.500" />
-          Implemented Debouncing to reduce API calls
-        </ListItem>
-      </List>
+      {exps.map(({ company, title, points, date }) => {
+        return (
+          <Box key={date} mb="2rem">
+            <Box
+              width={["100%", "100%", "40rem"]}
+              pl="2rem"
+              position="relative"
+            >
+              <Text fontSize="2xl">{company}</Text>
+              <Text opacity="0.6">
+                {title} <i>{date}</i>
+              </Text>
+              <Box
+                position="absolute"
+                top="9%"
+                left={0}
+                border="6px solid var(--primary-color)"
+                borderRadius="50%"
+                height="1.5rem"
+                width="1.5rem"
+              />
+            </Box>
+            <List spacing={1} ml="3rem" mt="1rem">
+              {points.map((ptn) => {
+                return (
+                  <ListItem key={ptn}>
+                    <ListIcon as={FiMinus} color="green.500" />
+                    {ptn}
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+        );
+      })}
     </Flex>
   );
 }
