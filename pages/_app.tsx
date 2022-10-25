ebd5a52e5ef9451eda0385a629c7a8823f925b8d
nishchay17 <nishchay13971@gmail.com>
2022-10-26 01:04:45 +0530

    fix(static): cleaned up static files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,38 +28,10 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Global
-        styles={`
-          @font-face {
-            font-family: 'Refinest';
-            font-style: normal;
-            font-display: swap;
-            src: url('./fonts/RefinestPersonalUe/RefinestPersonalUe-DOPK9.otf') format('opentype');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-          }
-        `}
-      />
       <Head>
         <meta name="description" content="Nishchay17 Portfolio" />
         <meta name="theme-color" content="#469895" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <link rel="icon" href="/svg/logo.svg" />
       </Head>
       <Script
         strategy="afterInteractive"
