2b8a2610bb297b41d420fa11248065a886f5744c
Nishchay <nishchay13971@gmail.com>
2023-11-19 13:43:22 +0530

    fix(testing): removed setup

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,7 +9,6 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const config = {
     // Add more setup options before each test is run
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     preset: 'ts-jest',
 }
