cc582ff3d951fc703f0edf77386ad620e42689ba
Nishchay <nishchay13971@gmail.com>
2023-11-19 16:11:53 +0530

    test(landing): added tests for landing page

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,23 +1,58 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import mockRouter from "next-router-mock";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
 
-import Home from "../app/page";
+import Hero from "../components/Landing/HeroSection";
+import Projects from "../components/Landing/Projects";
+import Tech from "../components/Landing/Tech";
+import { projects } from "../config/project";
+import TECH from "../config/tech";
+
+jest.mock("next/router", () => jest.requireActual("next-router-mock"));
+
+beforeAll(() => {
+  const intersectionObserverMock = () => ({
+    observe: () => null,
+    unobserve: () => null,
+    disconnect: () => null,
+  });
+  window.IntersectionObserver = jest
+    .fn()
+    .mockImplementation(intersectionObserverMock);
+});
 
 describe("Home Page tests", () => {
-  it("renders the heading", () => {
-    const intersectionObserverMock = () => ({
-      observe: () => null,
-      unobserve: () => null,
-      disconnect: () => null,
-    });
-    window.IntersectionObserver = jest
-      .fn()
-      .mockImplementation(intersectionObserverMock);
-
-    render(<Home />);
+  it("renders the hero heading", () => {
+    render(<Hero />);
     const heading = screen.getByRole("heading", {
       name: /Hello there I am Nishchay/i,
     });
     expect(heading).toBeInTheDocument();
+  });
+  it("renders all the projects", () => {
+    render(<Projects projects={[...projects]} />);
+    projects.forEach((project) => {
+      const heading = screen.getByText(project.name);
+      expect(heading).toBeInTheDocument();
+    });
+  });
+  it("redirects to project page on chick", () => {
+    mockRouter.push("/");
+    render(<Projects projects={[{ ...projects[0] }]} />, {
+      wrapper: MemoryRouterProvider,
+    });
+    const link = screen.getByRole("link");
+
+    expect(link).toBeInTheDocument();
+    fireEvent.click(link);
+    expect(mockRouter.asPath).toEqual(`/project/${projects[0].id}`);
+  });
+  it("renders all the technologies", () => {
+    render(<Tech techs={[...TECH]} />);
+    TECH.forEach((tech) => {
+      const techText = screen.getByText(tech);
+      expect(techText).toBeInTheDocument();
+    });
   });
 });
