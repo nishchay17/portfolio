72eafe9221103f8316457d65e8ae95869279fac7
Nishchay <nishchay13971@gmail.com>
2024-02-25 17:44:52 +0530

    feat: enabled dark mode

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { ThemeProvider } from "next-themes";
+
 function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
+    </>
+  );
 }
 
 export default Providers;
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <Analytics />
       <body className={notoSans.className}>{children}</body>
     </html>
