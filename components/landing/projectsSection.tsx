d73727d7e2923bdc980e47099fe2c9c61a06333e
Nishchay <nishchay13971@gmail.com>
2024-03-02 16:50:02 +0530

    feat: added new project section

--- components/landing/projectsSection.tsx
+++ components/landing/projectsSection.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useScroll, useTransform, motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
+import Image from "next/image";
+import { useScroll, useTransform, motion } from "framer-motion";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
 import { projects } from "@/config/project";
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
-import { useRef } from "react";
 
 const bagdeClass = {
   "No longer supported": "bg-red-200",
@@ -29,7 +30,6 @@ export default function Projects() {
   const horizontalScroll = useScroll({
     target: targetRef,
   });
-
   const x = useTransform(
     horizontalScroll.scrollYProgress,
     [0, 1],
@@ -38,7 +38,7 @@ export default function Projects() {
 
   return (
     <div className="border-t-[0.5px] border-white/20">
-      <div className="overflow-hidden whitespace-nowrap px-2 mt-4 select-none">
+      <div className="overflow-hidden whitespace-nowrap px-2 my-4 select-none">
         <motion.div style={{ translateX }}>
           {Array(40)
             .fill("Show case")
@@ -49,58 +49,96 @@ export default function Projects() {
             ))}
         </motion.div>
       </div>
-      <div className={`"relative h-[calc(40vh*12)]`} ref={targetRef}>
+      <div className="relative h-[calc(40vh*12)]" ref={targetRef}>
         <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-          <motion.div style={{ x }} className="flex gap-6">
-            <div className="pr-40">
-              <h2 className="text-7xl font-semibold mb-8">Projects</h2>
-              <p className="text-xl opacity-80">A collection of projects</p>
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_300px_at_5%_30%,#3e3e3e,transparent)]"></div>
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:16px_16px]"></div>
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_320px_at_90%_65%,#3e3e3e76,transparent)]"></div>
+          <motion.div style={{ x }} className="flex gap-16">
+            <div className="pr-32 flex flex-col">
+              <h2 className="text-7xl font-medium mb-8">Projects</h2>
+              <p className="text-xl opacity-80">
+                A showcase of all my work, <br />
+                mainly created in Next js, and javacript
+              </p>
+              <Link
+                className="mt-auto w-fit"
+                href={"https://www.linkedin.com/in/nishchay-trivedi-61219978"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button size="lg" className="uppercase font-extrabold">
+                  Contact me
+                </Button>
+              </Link>
             </div>
             {projects.map((project) => (
               <div
                 key={project.id}
-                className="w-[40vw] h-[70vh] border border-white/20 bg-zinc-950 rounded py-5 px-6"
+                className={
+                  "w-[40vw] h-[70vh] flex items-end justify-center relative"
+                }
               >
-                <div className="flex items-start justify-between gap-4 flex-col md:flex-row">
-                  <p className="text-3xl">{project.name}</p>
-                  {project.additionalTags?.map((tag) => (
-                    <Badge
-                      key={tag}
-                      className={cn(
-                        bagdeClass[tag as keyof typeof bagdeClass] ?? "",
-                        "flex-shrink-0"
-                      )}
-                    >
-                      {tag}
-                    </Badge>
-                  ))}
-                </div>
-                <p className="text-lg mt-4 text-primary/80 line-clamp-3">
-                  {project.description}
-                </p>
-                <div className="flex gap-2 mt-4">
-                  <Link
-                    href={project.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    prefetch={false}
-                  >
-                    <Button size="sm" variant="outline">
-                      Github
-                    </Button>
-                  </Link>
-                  <Link
-                    href={project.live}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    prefetch={false}
-                  >
-                    <Button size="sm" variant="outline">
-                      Live
-                    </Button>
-                  </Link>
+                <div
+                  className={
+                    "w-[98%] h-[98%] hover:size-full transition-all ease-in-out duration-500 rounded-xl" +
+                    " py-5 px-6 border-[0.5px] border-white/20 bg-transparent cursor-pointer relative"
+                  }
+                >
+                  <div className="flex items-center justify-between gap-4 mt-4 flex-col md:flex-row">
+                    <p className="text-3xl">{project.name}</p>
+                    <div className="flex items-center">
+                      <Link
+                        href={project.github}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Button
+                          className="group uppercase tracking-wider text-lg transition-all duration-700"
+                          variant="link"
+                          size="sm"
+                        >
+                          <span className="group-hover:italic">github</span>
+                          <ArrowTopRightIcon className="ml-1 size-6 group-hover:size-7" />
+                        </Button>
+                      </Link>
+                      <Link
+                        href={project.live}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Button
+                          className="group uppercase tracking-wider text-lg transition-all"
+                          variant="link"
+                          size="sm"
+                        >
+                          <span className="group-hover:italic">Visit</span>
+                          <ArrowTopRightIcon className="ml-1 w-6 h-6 group-hover:w-7 group-hover:h-7" />
+                        </Button>
+                      </Link>
+                    </div>
+                  </div>
+                  {[project.createdOn, ...(project.additionalTags ?? [])]?.map(
+                    (tag) => (
+                      <Badge
+                        key={tag}
+                        className={cn(
+                          bagdeClass[tag as keyof typeof bagdeClass] ??
+                            "bg-yellow-200",
+                          "flex-shrink-0 mt-2 mr-2"
+                        )}
+                      >
+                        {tag}
+                      </Badge>
+                    )
+                  )}
+                  <p className="text-lg mt-4 text-primary/80 line-clamp-3">
+                    {project.description}
+                  </p>
+                  <div className="absolute inset-0 size-full bg-zinc-950/10 backdrop-blur-xl -z-20" />
                 </div>
                 <Image
+                  className="absolute -z-10 bottom-0 group right-1/2 translate-x-1/2 w-[80%] rounded-t-xl"
                   src={`/img/${project.image}`}
                   alt={project.name}
                   width={800}
