6a3b3bc3c5c226cfa4fefdd15f04de317a0674a0
Nishchay <nishchay13971@gmail.com>
2024-04-18 02:55:15 +0530

    feat: new skill design

--- components/landing/techSection.tsx
+++ components/landing/techSection.tsx
@@ -14,27 +14,30 @@ function Tech() {
   }
   return (
     <section
-      className="container px-6 py-20 md:py-0 md:px-12 min-h-screen flex lg:-mt-10"
+      className="container px-6 pt-48 pb-24 md:px-12 flex flex-col"
       id="skills"
       ref={ref}
     >
-      <div className="max-w-[1400px] m-auto">
-        <StaggerText text="Skills" className="text-heading font-medium mb-10" />
-        <div className="flex flex-wrap gap-4 md:gap-6 mb-10">
-          {TECH.map((it) => (
-            <div
-              className="border px-4 md:px-8 py-4 rounded-lg hover:border-white/20 hover:shadow-inner transition-all"
-              key={it.id}
-            >
-              <p className="underline underline-offset-2 opacity-80 mb-2 select-none">
+      <StaggerText text="Skills" className="text-heading font-medium mb-10" />
+      <div className="flex flex-col gap-4 md:gap-6 mb-10">
+        {TECH.map((it) => (
+          <div
+            className="border-b py-4 hover:border-white/25 group transition-all"
+            key={it.id}
+          >
+            <div className="h-7 overflow-hidden">
+              <p className="opacity-90 mb-2 text-xl select-none -translate-y-14 group-hover:translate-y-0 transition-all duration-500">
                 {it.name}
               </p>
-              <p className="text-para mb-1">
-                {it.techs.map((it) => it).join(", ")}
+              <p className="opacity-50 mb-2 text-xl select-none -translate-y-9 group-hover:translate-y-0 transition-all duration-500">
+                {it.name}
               </p>
             </div>
-          ))}
-        </div>
+            <p className="text-para group-hover:text-yellow-400 transition-all duration-500">
+              {it.techs.map((it) => it).join(", ")}
+            </p>
+          </div>
+        ))}
       </div>
     </section>
   );
